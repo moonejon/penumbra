8193e69c446b89a74763a955e40feec2ba9868d6
Jonathan Mooney <moonejon@gmail.com>
2025-05-09 14:22:52 -0500

    rudimentary UI work for book search

--- components/library/search-book.tsx
+++ components/library/search-book.tsx
@@ -2,6 +2,8 @@
 
 import { useActionState } from "react"
 import { fetchMetadataAction } from "@/app/actions"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 export function SearchBook({}: {}) {
 
@@ -12,13 +14,16 @@ export function SearchBook({}: {}) {
 
     const [state, formAction] = useActionState(fetchMetadataAction, initialState)
     return (
-        <div>
-        <form action={formAction}>
-            <input type="text" name="isbn"/>
-            <button type="submit">Search Book</button>
+        <div className="flex flex-col">
+        <form action={formAction} className="flex flex-col items-end space-y-5">
+            <Input type="text" name="isbn" id="isbn" placeholder="ISBN number"/>
+            <Button type="submit" className="max-w-24">Search Book</Button>
         </form>
         <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
+        <h2 className="font-bold text-2xl mb-4">Book details</h2>
+        
+        <img src={state?.book.image} />
+
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           {JSON.stringify(state?.book, null, 2)}
         </pre>
