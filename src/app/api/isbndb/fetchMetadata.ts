3e35aeb5dcd44468f843dbd2c456866ee1ce6610
Jonathan Mooney <moonejon@gmail.com>
2025-07-20 21:05:16 -0500

    removing unnecessary console logs

--- src/app/api/isbndb/fetchMetadata.ts
+++ src/app/api/isbndb/fetchMetadata.ts
@@ -1,12 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const fetchMetadata = async (isbn: string): Promise<any> => {
   const headers = new Headers();
-  console.log("HELLO")
-  console.log(process.env.NEXT_PUBLIC_ISBN_DB_API_KEY)
   headers.set("Authorization", `${process.env.NEXT_PUBLIC_ISBN_DB_API_KEY}`);
   headers.set("Content-Type", "application/json");
 
-  console.log(headers)
   const response = await fetch(`https://api2.isbndb.com/book/${isbn}`, {
     method: "GET",
     headers: headers,
