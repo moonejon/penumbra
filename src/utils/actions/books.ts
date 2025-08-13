e2c61c7f0c895fa41290fa6f5b6eea3c5fb55038
Jonathan Mooney <moonejon@gmail.com>
2025-08-12 19:47:50 -0500

    an attempt was made to implement cursor-based pagination

--- src/utils/actions/books.ts
+++ src/utils/actions/books.ts
@@ -1,10 +1,10 @@
 "use server";
 
 import { auth } from "@clerk/nextjs/server";
-import prisma  from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { BookType } from "@/shared.types";
 
-export async function importBooks( importQueue: BookType[]) {
+export async function importBooks(importQueue: BookType[]) {
   const { userId } = await auth();
 
   if (!userId) {
@@ -20,10 +20,10 @@ export async function importBooks( importQueue: BookType[]) {
   }
 
   return await prisma.book.createMany({
-    data: importQueue.map(book => ({
-        ...book,
-        ownerId: user.id
-    }))
+    data: importQueue.map((book) => ({
+      ...book,
+      ownerId: user.id,
+    })),
   });
 }
 
@@ -44,7 +44,65 @@ export async function fetchBooks() {
 
   return await prisma.book.findMany({
     where: {
-      ownerId: user.id
-    }
-  })
+      ownerId: user.id,
+    },
+  });
+}
+
+export async function fetchBooksPaginated({
+  pageSize,
+  cursor,
+  skip,
+}: {
+  pageSize: number;
+  cursor: number;
+  skip: number;
+}) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { clerkId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found in database");
+  }
+
+  const filters = {
+    ownerId: user.id,
+  }
+
+  // useEffect(() => {
+
+  // })
+
+  if (cursor) {
+    const remainingPagesOfResults = await prisma.book.findMany({
+      take: pageSize ?? 10,
+      skip: skip ?? 1,
+      where: filters,
+      cursor: {
+        id: cursor,
+      },
+    });
+  } else {
+    const [firstPageOfResults, totalCount] = await prisma.$transaction([
+    prisma.book.findMany({
+      take: pageSize,
+      where: filters,
+      orderBy: {
+        id: "asc",
+      },
+    }),
+    prisma.book.count({
+      where: filters,
+    }),
+    ]);
+  }
+
+  return {
 }
