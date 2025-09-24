af1f8b269a66c83905ed95eb161dfae0383e6dd8
Jonathan Mooney <moonejon@gmail.com>
2025-09-23 22:50:45 -0500

    making library view public

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -6,23 +6,39 @@ import _ from "lodash";
 export default async function LibraryPage({
   searchParams,
 }: {
-  searchParams: Promise<{ page?: string, title?: string, authors?: string, subjects?: string }>;
+  searchParams: Promise<{
+    page?: string;
+    title?: string;
+    authors?: string;
+    subjects?: string;
+  }>;
 }) {
-
   const params = await searchParams;
 
   const { title, authors, subjects } = params;
   const page = Number(params.page) || 1;
 
+  const result = await fetchBooksPaginated({
+    page: page,
+    title: title,
+    authors: authors,
+    subjects: subjects,
+  });
 
-  const result = await fetchBooksPaginated({ page: page, title: title, authors: authors, subjects: subjects });
+  const filterData = await fetchFilters();
 
-  const filterData = await fetchFilters()
-
-  const sortedAuthors = _.uniq(filterData.flatMap(el => el.authors ).sort())
-  const sortedSubjects = _.uniq(filterData.flatMap(el => el.subjects ).sort())
+  const sortedAuthors = _.uniq(filterData.flatMap((el) => el.authors).sort());
+  const sortedSubjects = _.uniq(filterData.flatMap((el) => el.subjects).sort());
 
   const { books, pageCount } = result;
 
-  return <Library books={books} authors={sortedAuthors} subjects={sortedSubjects} page={page} pageCount={pageCount} />;
+  return (
+    <Library
+      books={books}
+      authors={sortedAuthors}
+      subjects={sortedSubjects}
+      page={page}
+      pageCount={pageCount}
+    />
+  );
 }
--- src/utils/actions/filters.ts
+++ src/utils/actions/filters.ts
@@ -1,27 +1,27 @@
 "use server";
 
-import { auth } from "@clerk/nextjs/server";
+// import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 
 export async function fetchFilters() {
-  const { userId } = await auth();
+  // const { userId } = await auth();
 
-  if (!userId) {
-    throw new Error("User not authenticated");
-  }
+  // if (!userId) {
+  //   throw new Error("User not authenticated");
+  // }
 
-  const user = await prisma.user.findUnique({
-    where: { clerkId: userId },
-  });
+  // const user = await prisma.user.findUnique({
+  //   where: { clerkId: userId },
+  // });
 
-  if (!user) {
-    throw new Error("User not found in database");
-  }
+  // if (!user) {
+  //   throw new Error("User not found in database");
+  // }
 
   return await prisma.book.findMany({
     select: {
-        authors: true,
-        subjects: true
-    }
-  })
+      authors: true,
+      subjects: true,
+    },
+  });
 }
